5891073e7798694d3cd95f5479ac1a908d5f2df9
Chun Win Yuen <cw2yuen@eceLinux1.uwaterloo.ca>
2010-05-19 00:30:32 -0400

    Commented files and added reading port from file for TCP.

--- P1/clientTCP.java
+++ P1/clientTCP.java
@@ -1,23 +1,40 @@
+/**
+* clientTCP.java
+* Author: Aaron Chun Win Yuen
+* Course: ECE428
+* Written in 2010
+*/
+
 import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public class clientTCP {
+/**
+* The client class that uses the TCP protocol to implement the World Cup players program. The program
+* makes use of a file that reads what port the serverTCP is listening to. At the end of the execution,
+* the client deletes that port file.
+*/
+public class clientTCP 
+{
+	// Constants.
 	private static final String IP = "127.0.0.1";
 	private static final int ARGS_LENGTH = 2;
-	private static final int PORT = 23984;
 	private static final int ERROR_CODE = 1;
 	private static final String OUTPUT_FILENAME = "outTCP.dat";
+	private static final String PORT_FILENAME = "portTCP.ini";
 	
+	/**
+	 * Main method.
+	 */
 	public static void main(String [] args)
 	{
 		System.out.println("RUNNING clientTCP");
 
-		// If the number of arguments do not match what is expected. Show the 
-		// program should be used.
+		// If the number of arguments do not match what is expected. Show how the 
+		// program should be used and exit the program.
 		if (args.length != ARGS_LENGTH)
 		{
-			ShowUsage();
+			showUsage();
 		}
 
 		// Store parameters from command line arguments.
@@ -26,43 +43,68 @@ public class clientTCP {
 		
 		// Variables used for TCP connection.
 		Socket socket = null;
-        PrintWriter out = null;
-        BufferedReader in = null;
+        PrintWriter toServer = null;
+        BufferedReader fromServer = null;
 		
 		ArrayList players = null;
+		
+		// Busy loop to wait for the port filename to exist. If it does, retrieve the port number.
+		int port = getPortNumber();
 		
 		// Get list of players and establish connection.
 		try
 		{
-			players = GetPlayers(filename);
+			players = getPlayers(filename);
 			
-			socket = new Socket(IP, PORT);
-			out = new PrintWriter(socket.getOutputStream(), true);
-			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			socket = new Socket(IP, port);
+			toServer = new PrintWriter(socket.getOutputStream(), true);
+			fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
 		
 			// Send country to server.
-			out.println(country);
+			toServer.println(country);
 			
 			// Send how many players to send.
-			out.println(players.size());
+			toServer.println(players.size());
 			
 			// Send list of players to server.
 			for (int i = 0; i < players.size(); i++)
 			{
 				
-				out.println(players.get(i).toString());
+				toServer.println(players.get(i).toString());
 			}
+
+			FileWriter fstream = new FileWriter(OUTPUT_FILENAME);
+			BufferedWriter toFile = new BufferedWriter(fstream);
 			
 			// Reading the number of players to receive from server.
-			int numPlayers = Integer.parseInt(in.readLine());
+			int numPlayers = Integer.parseInt(fromServer.readLine());
 
-			// Reader the player.
-			for(int i = 0; i < numPlayers; i++)
+			// If there are no players returned for the country. Client expects
+			// a message from the server (one line only).
+			if(numPlayers == 0)
 			{
-				String player = in.readLine();
-				System.out.println(i + ": " + player);
+				String msg = fromServer.readLine();
+				System.out.println(msg);
+				toFile.write(msg);
 			}
+			// Otherwise, MORE HERE
+			else 
+			{
+				// Read the players that are returned by the server.
+				for(int i = 0; i < numPlayers; i++)
+				{
+					String player = fromServer.readLine();
+					System.out.println(i + ": " + player);
+					toFile.write(player);
+				}
+			}
+			
+			// Close everything before program ends.
+			socket.close();
+			toServer.close();
+			fromServer.close();
+			toFile.close();
 		}
 		catch (UnknownHostException e)
 		{
@@ -78,13 +120,27 @@ public class clientTCP {
 		System.out.println("STOPPING clientTCP");		
 	}
 
-	private static void ShowUsage()
+	/**
+	 * Display a usage message to inform the user how to use this program in the command line.
+	 * This method also exits the program as well.
+	 */
+	private static void showUsage()
 	{
 		System.err.println("USAGE: java clientTCP <input filename> <country>");
 		System.exit(ERROR_CODE);
 	}
 
-	private static ArrayList GetPlayers(String filename) throws IOException
+	/**
+	 * Retrieve a list of players based on the given filename.
+	 *
+	 * Args:
+	 *	 filename			The filename of the input file.
+	 *
+	 * Returns:				A list of players that are present in the input file.
+	 *
+	 * Throws:				IOException
+	 */
+	private static ArrayList getPlayers(String filename) throws IOException
 	{
 		ArrayList players = new ArrayList();
 		
@@ -95,5 +151,32 @@ public class clientTCP {
 			players.add(line);
 		}
 		return players;
+	}
+	
+	/*
+	 * Busy loop to wait for the port filename to exist. If it does, the method retrieves the port number.
+	 */
+	private static int getPortNumber()
+	{
+		int port = -1;
+		try
+		{
+			File f = null;
+			while(true)
+			{
+				f = new File(PORT_FILENAME);
+				if(f.exists())
+				{
+					String line = new BufferedReader(new FileReader(PORT_FILENAME)).readLine();
+					port = Integer.parseInt(line);
+					break;
+				}
+			}
+		}
+		catch (IOException e)
+		{
+			System.err.println("[ERROR] Error getting port number.");
+		}
+		return port;
 	}
 }
--- P1/serverTCP.java
+++ P1/serverTCP.java
@@ -1,79 +1,184 @@
-import java.net.*;
+/**
+* serverTCP.java
+* Author: Ka Man Sophia Wong
+* Course: ECE428
+* Written in 2010
+*/
+
 import java.io.*;
+import java.net.*;
 import java.util.*;
 
-public class serverTCP {
-	public static void main( String args[] ) throws IOException {
-		ServerSocket serverSocket = null;
+/**
+* The server class that retrieves a country and a list of players from clientTCP and returns back to the client
+* only the players that are playing for that country using the TCP protocol. To establish a connection, the
+* server randomly selects a port, and writes this port number to a file, expecting the client to read the port
+* number off this file.
+*/
+public class serverTCP 
+{
+	// Constants.
+	private static final String IP = "127.0.0.1";
+	private static final int ARGS_LENGTH = 0;
+	private static final int ERROR_CODE = 1;
+	private static final String PORT_FILENAME = "portTCP.ini";
 
-		try {
+	/**
+	 * Main method.
+	 */
+	public static void main( String args[] ) throws IOException {
+		System.out.println("RUNNING clientTCP");
+
+		// If the number of arguments do not match what is expected. Show how the 
+		// program should be used and exit the program.
+		if (args.length != ARGS_LENGTH)
+		{
+			showUsage();
+		}
+		
+		ServerSocket serverSocket = null;
+		try 
+		{
             serverSocket = new ServerSocket();
-	    serverSocket.bind( new InetSocketAddress("127.0.0.1", 23984));
-        } catch (IOException e) {
-            System.err.println("Could not listen on port: " + e.getMessage());
-            System.exit(1);
+			serverSocket.bind(null);
         }
+		catch (IOException e)
+		{
+            System.err.println("[ERROR] Could not listen on port: " + e.getMessage());
+            System.exit(ERROR_CODE);
+        }
+		
+		// Write the port to the port filename so that the client knows which port to use to
+		// establish the connection.
+		writePortNumber(serverSocket.getLocalPort());
+		
 
         Socket clientSocket = null;
-        try {
+        try 
+		{
             clientSocket = serverSocket.accept();
-        } catch (IOException e) {
-            System.err.println("Accept failed.");
-            System.exit(1);
+        }
+		catch (IOException e)
+		{
+            System.err.println("[ERROR] Accept failed.");
+            System.exit(ERROR_CODE);
         }
         
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-	try
-	{  
-		String country = in.readLine();
-		int numOfLines = Integer.parseInt( in.readLine() );
+		try
+		{  
+			String country = in.readLine();
+			int numOfLines = Integer.parseInt( in.readLine() );
 
-		// Check if country is found by checking if there are number of plaeyrs
-		ArrayList listOfPlayers = new ArrayList();
-		
-		for( int x = 0; x < numOfLines; x++ ) {
-			listOfPlayers.add(in.readLine());
-		}
-
-		//process the players
-		ArrayList processedPlayers = getPlayersInCountry( country, listOfPlayers );
-		out.println( processedPlayers.size() );
-
-		if( processedPlayers.size() > 0 ) { 
-			for( int x = 0; x < processedPlayers.size(); x++ ) {
-				out.println( processedPlayers.get(x) );
+			// Check if country is found by checking if there are number of plaeyrs.
+			ArrayList listOfPlayers = new ArrayList();
+			
+			for( int x = 0; x < numOfLines; x++ )
+			{
+				listOfPlayers.add(in.readLine());
 			}
-		} else { 
-			out.println( country + " did not qualify to the world cup" );
-		}
+
+			// Process the players.
+			ArrayList processedPlayers = getPlayersInCountry( country, listOfPlayers );
+			out.println( processedPlayers.size() );
+
+			if( processedPlayers.size() > 0 )
+			{ 
+				for( int x = 0; x < processedPlayers.size(); x++ )
+				{
+					out.println( processedPlayers.get(x) );
+				}
+			} 
+			else
+			{ 
+				out.println( country + " did not qualify to the world cup" );
+			}
 
         }
         catch(NumberFormatException nfe)
         {
-        	System.out.println("Numbers not intergers");
-        	out.println("Numbers not intergers");
+        	System.err.println("[ERROR] Numbers not intergers.");
         }
-        
-        
+		
+		// Close everything before program ends.
         out.close();
         in.close();
         clientSocket.close();
         serverSocket.close();
-    }
-
-    private static ArrayList getPlayersInCountry( String country, ArrayList listOfPlayers )
-    {
-	ArrayList listOfPlayersInCountry = new ArrayList();
-	// Iterate through each player and store the ones in the specified country 
-	for( int x = 0; x < listOfPlayers.size(); x++ ) {
-		String[] playerAndCountry = ((String)listOfPlayers.get(x)).split(" ");
-		if( playerAndCountry[1].equals( country ) ) {
-			listOfPlayersInCountry.add( playerAndCountry[0] );
+		
+		// Delete the port file
+		File file = new File(PORT_FILENAME);
+		if (file.exists())
+		{
+			file.delete();
 		}
+    }
+	
+	/**
+	 * Display a usage message to inform the user how to use this program in the command line.
+	 * This method also exits the program as well.
+	 */
+	private static void showUsage()
+	{
+		System.err.println("USAGE: java serverTCP");
+		System.exit(ERROR_CODE);
 	}
 
-	return listOfPlayersInCountry;
-    }	
+	/**
+	 * Given a list of players and a country, this method returns a list of only the players from that country.
+	 * 
+	 * Args:
+	 *   country					The country to filter the list of players.
+	 *   listOfPlayers				The list of players used to filter.
+	 *
+	 * Returns:						A list of players from the given list that are from the specified country.
+	 */
+    private static ArrayList getPlayersInCountry( String country, ArrayList listOfPlayers )
+    {
+		ArrayList listOfPlayersInCountry = new ArrayList();
+		
+		// Iterate through each player and store the ones in the specified country 
+		for( int x = 0; x < listOfPlayers.size(); x++ )
+		{
+			String[] playerAndCountry = ((String)listOfPlayers.get(x)).split(" ");
+			if( playerAndCountry[1].equals( country ) )
+			{
+				listOfPlayersInCountry.add( playerAndCountry[0] );
+			}
+		}
+
+		return listOfPlayersInCountry;
+    }
+
+	/**
+	 * Write the port to the port filename so that the client knows which port to use to
+     * establish the connection.
+	 *
+	 * Args:
+	 *   port				The port to write to port file. 
+	 */
+	private static void writePortNumber(int port)
+	{
+		try
+		{
+			File file = new File(PORT_FILENAME);
+			if(file.exists())
+			{
+				file.delete();
+				System.out.println("file deleted first");
+			}
+			
+			FileWriter fstream = new FileWriter(PORT_FILENAME);
+			BufferedWriter toPortFile = new BufferedWriter(fstream);
+			
+			toPortFile.write(String.valueOf(port));
+			toPortFile.close();
+		}
+		catch (IOException e)
+		{
+			System.err.println("[ERROR] Cannot write port to port file");
+		}
+	}
 }
