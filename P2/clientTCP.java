263251043117c1e7509039e77ecbad6a3c30886c
John Huang <j13huang@eceLinux3.uwaterloo.ca>
2010-06-15 23:17:43 -0400

    added testing files

---
+++ P2/clientTCP.java
@@ -0,0 +1,183 @@
+/**
+* clientTCP.java
+* Author: Aaron Chun Win Yuen
+* Course: ECE428
+* Written in 2010
+*/
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+/**
+* The client class that uses the TCP protocol to implement the World Cup players program. The program
+* makes use of a file that reads what port the serverTCP is listening to. At the end of the execution,
+* the client deletes that port file.
+*/
+public class clientTCP 
+{
+	// Constants.
+	private static final String IP = "127.0.0.1";
+	private static final int ERROR_CODE = 1;
+	private static final String OUTPUT_FILENAME = "outTCP.dat";
+	private static final String PORT_FILENAME = "portTCP.ini";
+	
+	/**
+	 * Main method.
+	 */
+	public static void main(String [] args)
+	{
+		// If the number of arguments do not match what is expected. Show how the 
+		// program should be used and exit the program.
+		if (args.length != 2)
+		{
+            System.err.println("USAGE: java clientTCP <input filename> <country>");
+            System.exit(ERROR_CODE);
+		}
+
+		// Store parameters from command line arguments.
+		String filename = args[0];
+		String country = args[1];
+		
+		// Variables used for TCP connection.
+		S_StreamSocket socket = null;
+		
+		ArrayList players = null;
+		
+		// Busy loop to wait for the port filename to exist. If it does, retrieve the port number.
+		int port = getPortNumber();
+		
+		// Get list of players and establish connection.
+		try
+		{
+			players = getPlayers(filename);
+			
+            InetSocketAddress socketAddress = new InetSocketAddress(IP, port);
+			socket = new S_StreamSocket(socketAddress);
+		
+			// Send country to server.
+            sendString(socket, country);
+			
+			// Send how many players to send.
+            int numPlayers = players.size();
+            sendData(socket, numPlayers);
+			
+			// Send list of players to server.
+			for (int i = 0; i < numPlayers; i++)
+			{
+                sendString(socket, players.get(i).toString());
+			}
+			
+			FileWriter fstream = new FileWriter(OUTPUT_FILENAME);
+			BufferedWriter toFile = new BufferedWriter(fstream);
+			
+			// Reading the number of players to receive from server.
+            int numPlayers = Integer.parseInt(receiveString(socket));
+
+			// If there are no players returned for the country. Client expects
+			// a message from the server (one line only).
+			if(numPlayers == 0)
+			{
+                String msg = receiveString(socket);
+				toFile.write(msg);
+			}
+			// Otherwise, read each player name from the server and write it out to outTCP.dat.
+			else 
+			{
+                String player = receiveString(socket);
+				toFile.write(player);
+				// Read the players that are returned by the server.
+				for(int i = 1; i < numPlayers; i++)
+				{
+                    player = receiveString(socket);
+					toFile.write("\n" + player);
+				}
+			}
+			
+			// Close everything before program ends.
+			socket.close();
+			toFile.close();
+		}
+		catch (UnknownHostException e)
+		{
+			System.err.println("Host cannot be found: " + e.getMessage());
+			System.exit(ERROR_CODE);
+		}
+		catch (IOException e)
+		{
+			System.err.println(e.getMessage());
+			System.exit(ERROR_CODE);
+		}
+	}
+
+    private static String receiveString(S_StreamSocket socket)
+    {
+        int len = 1024;
+        byte[] buf = new byte[len];
+
+        int receivedBytes = socket.S_receive(buf, len);
+
+        return new String(buf, 0, receivedBytes);
+    }
+
+    private static void sendData(S_StreamSocket socket, int data)
+    {
+        sendString(Integer.toString(data));
+    }
+
+    private static void sendString(S_StreamSocket socket, String data)
+    {
+        byte[] buf = socket.getBytes();
+        socket.S_send(buf, buf.length);
+    }
+
+	/**
+	 * Retrieve a list of players based on the given filename.
+	 *
+	 * Args:
+	 *	 filename			The filename of the input file.
+	 *
+	 * Returns:				A list of players that are present in the input file.
+	 *
+	 * Throws:				IOException
+	 */
+	private static ArrayList getPlayers(String filename) throws IOException
+	{
+		ArrayList players = new ArrayList();
+		
+		BufferedReader in = new BufferedReader(new FileReader(filename));
+		String line;
+		while ((line = in.readLine()) != null)
+		{
+			players.add(line);
+		}
+		return players;
+	}
+	
+	/**
+	 * Busy loop to wait for the port filename to exist. If it does, the method retrieves the port number.
+	 */
+	private static int getPortNumber()
+	{
+		int port = -1;
+		try
+		{
+			File f = null;
+			while(true)
+			{
+				f = new File(PORT_FILENAME);
+				if(f.exists())
+				{
+					String line = new BufferedReader(new FileReader(PORT_FILENAME)).readLine();
+					port = Integer.parseInt(line);
+					break;
+				}
+			}
+		}
+		catch (IOException e)
+		{
+			System.err.println("[ERROR] Error getting port number.");
+		}
+		return port;
+	}
+}
---
+++ P2/serverTCP.java
@@ -0,0 +1,185 @@
+/**
+* serverTCP.java
+* Author: Ka Man Sophia Wong
+* Course: ECE428
+* Written in 2010
+*/
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+/**
+* The server class that retrieves a country and a list of players from clientTCP and returns back to the client
+* only the players that are playing for that country using the TCP protocol. To establish a connection, the
+* server randomly selects a port, and writes this port number to a file, expecting the client to read the port
+* number off this file.
+*/
+public class serverTCP 
+{
+	// Constants.
+	private static final String PORT_FILENAME = "portTCP.ini";
+	private static final int ERROR_CODE = 1;
+
+	/**
+	 * Main method.
+	 */
+	public static void main( String args[] ) throws IOException {
+        if (args.length != 0) {
+             System.out.println("USAGE: java serverTCP");
+             return;
+        }
+		
+		S_StreamSocket socket = null;
+		try 
+		{
+            socket = new S_StreamSocket();
+			//socket.bind(null);
+        }
+		catch (IOException e)
+		{
+            System.err.println("[ERROR] Could not listen on port: " + e.getMessage());
+            System.exit(ERROR_CODE);
+        }
+		
+		// Write the port to the port filename so that the client knows which port to use to
+		// establish the connection.
+        InetSocketAddress localAddress = socket.S_getLocalSocketAddress();
+		writePortNumber(localAddress.getPort());
+
+        InetSocketAddress clientSocket = null;
+        try 
+		{
+            clientSocket = socket.S_accept();
+        }
+		catch (IOException e)
+		{
+            System.err.println("[ERROR] Accept failed.");
+            System.exit(ERROR_CODE);
+        }
+        
+		try
+		{  
+            // get country
+            String country = receiveString(socket);
+            int numLines = Integer.parseInt(receiveString(socket));
+
+			// Check if country is found by checking if there are number of plaeyrs.
+			ArrayList listOfPlayers = new ArrayList();
+			
+			for( int x = 0; x < numOfLines; x++ )
+			{
+				listOfPlayers.add(receiveString(socket));
+			}
+
+			// Process the players.
+			ArrayList processedPlayers = getPlayersInCountry( country, listOfPlayers );
+            int numPlayers = processedPlayers.size();
+            sendData(socket, numPlayers);
+
+			if( numPlayers > 0 )
+			{ 
+				for( int i = 0; i < numPlayers; i++ )
+				{
+                    sendString(socket, (String)(processedPlayers.get(i)));
+				}
+			} 
+			else
+			{ 
+                sendString(socket, country + " did not qualify for the world cup");
+			}
+
+        }
+        catch(NumberFormatException nfe)
+        {
+        	System.err.println("[ERROR] Numbers not integers.");
+        }
+		
+		// Close everything before program ends.
+        socket.S_close();
+        clientSocket.close();
+		
+		// Delete the port file
+		File file = new File(PORT_FILENAME);
+		if (file.exists())
+		{
+			file.delete();
+		}
+    }
+
+    private static String receiveString(S_StreamSocket socket)
+    {
+        int len = 1024;
+        byte[] buf = new byte[len];
+
+        int receivedBytes = socket.S_receive(buf, len);
+
+        return new String(buf, 0, receivedBytes);
+    }
+
+    private static void sendData(S_StreamSocket socket, int data)
+    {
+        sendString(Integer.toString(data));
+    }
+
+    private static void sendString(S_StreamSocket socket, String data)
+    {
+        byte[] buf = socket.getBytes();
+        socket.S_send(buf, buf.length);
+    }
+	
+	/**
+	 * Given a list of players and a country, this method returns a list of only the players from that country.
+	 * 
+	 * Args:
+	 *   country					The country to filter the list of players.
+	 *   listOfPlayers				The list of players used to filter.
+	 *
+	 * Returns:						A list of players from the given list that are from the specified country.
+	 */
+    private static ArrayList getPlayersInCountry( String country, ArrayList listOfPlayers )
+    {
+		ArrayList listOfPlayersInCountry = new ArrayList();
+		
+		// Iterate through each player and store the ones in the specified country 
+		for( int x = 0; x < listOfPlayers.size(); x++ )
+		{
+			String[] playerAndCountry = ((String)listOfPlayers.get(x)).split(" ");
+			if( playerAndCountry[1].equals( country ) )
+			{
+				listOfPlayersInCountry.add( playerAndCountry[0] );
+			}
+		}
+
+		return listOfPlayersInCountry;
+    }
+
+	/**
+	 * Write the port to the port filename so that the client knows which port to use to
+     * establish the connection.
+	 *
+	 * Args:
+	 *   port				The port to write to port file. 
+	 */
+	private static void writePortNumber(int port)
+	{
+		try
+		{
+			File file = new File(PORT_FILENAME);
+			if(file.exists())
+			{
+				file.delete();
+			}
+			
+			FileWriter fstream = new FileWriter(PORT_FILENAME);
+			BufferedWriter toPortFile = new BufferedWriter(fstream);
+			
+			toPortFile.write(String.valueOf(port));
+			toPortFile.close();
+		}
+		catch (IOException e)
+		{
+			System.err.println("[ERROR] Cannot write port to port file");
+		}
+	}
+}
