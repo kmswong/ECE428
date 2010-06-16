065b74f4157d304b1e0f964e3e1eaf7f87cca868
Chun Win Yuen <cw2yuen@eceLinux1.uwaterloo.ca>
2010-06-15 22:37:54 -0400

    Added ID and MF in header. Need to implement toString

--- P2/S_StreamPacket.java
+++ P2/S_StreamPacket.java
@@ -5,16 +5,24 @@ public class S_StreamPacket {
 	private int m_acknowledgementNumber;
 	private int m_checksum;
 	private int m_currentState;
+	private boolean m_mf;
+	private int m_id;
 		
 	private byte[] m_data;
 	
-	public S_StreamPacket( int s, int sequenceNumber, int ackNumber, int checksum, byte[] data )
+	public S_StreamPacket( int id, int s, int sequenceNumber, int ackNumber, int checksum, byte[] data, boolean mf )
 	{
+		this.m_id = id;
 		this.m_currentState = s;
 		this.m_sequenceNumber = sequenceNumber;
 		this.m_acknowledgementNumber = ackNumber;
 		this.m_checksum = checksum;
 		this.m_data = data;
+		this.m_mf = mf;
+	}
+	
+	public int getId() {
+		return this.m_id;
 	}
 	
 	public int getState(){
@@ -39,5 +47,15 @@ public class S_StreamPacket {
 	public byte[] getData()
 	{
 		return this.m_data;
+	}
+	
+	public boolean getMf()
+	{
+		return this.m_mf;
+	}
+	
+	public String toString()
+	{
+		return "";
 	}
 }
