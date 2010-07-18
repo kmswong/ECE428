962ce3974c61ec527d95d1446d06c5f7c43ae817
Chun Win Yuen <cw2yuen@eceLinux1.uwaterloo.ca>
2010-07-18 12:35:27 -0400

    Added more changes to P3, including word and char checking.

--- P3/p3server.java
+++ P3/p3server.java
@@ -18,17 +18,19 @@ public class p3server
 	private static final String TXT_KEY_CHANGE = "KEY CHANGE";
 	private static final String FILENAME_WORDS = "hamlet_word_out.txt";
 	private static final String FILENAME_CHARS = "hamlet_char_out.txt";
+	private static final String FILENAME_OUTDAT = "out.dat";
 	private static final int NUM_WORD_CHECK = 5;
 	private static final int NUM_CHAR_CHECK = 10;
 	
 	public static void main(String[] args) throws IOException {
-	
 		int f = Integer.parseInt(args[0]);
 		int n = Integer.parseInt(args[1]);
 		DatagramSocket serverSocket = null;
 		
 		HashMap<String, Integer> wordMap = getWordMap(FILENAME_WORDS);
 		HashMap<String, Integer> charMap = getWordMap(FILENAME_CHARS);
+		// HACK: add empty space.
+		charMap.put(" ", 0);
 		
 		try 
 		{
@@ -51,6 +53,11 @@ public class p3server
 		} catch (Exception e) {
 			System.err.println( e.getMessage() );
 		}
+		
+		// Open File for out.dat
+		File file = new File(FILENAME_OUTDAT);
+		FileWriter fstream = new FileWriter(FILENAME_OUTDAT);
+		BufferedWriter toOutputFile = new BufferedWriter(fstream);
 
 		System.out.println("Receiving~~");
 
@@ -101,34 +108,62 @@ public class p3server
 					byte[] deciphered = c.doFinal( byteToDecrypt  );
 					String decipheredStr = new String( deciphered );
 					
-					if( pCheck( deciphered ) ) {
+					//System.out.println();
+					//System.out.println("Deciphered Text *************************************");					
+					//System.out.println(decipheredStr);
+					
+					boolean check = pCheck( deciphered );
+					System.out.println("pCheck: " + check);
+					
+					// Do char and word checking.
+
+					if( check ) {
+						boolean wordCheck = true;
+						boolean charCheck = true;
 						int decipheredStrSize = decipheredStr.length();
 						for (int i  = 0; i < NUM_CHAR_CHECK && i < decipheredStrSize; ++i) {
 							String decipheredChar = String.valueOf(decipheredStr.charAt(i));
-							if (charMap.get(decipheredChar) == null) {
+							if (!charMap.containsKey(decipheredChar)) {
 								//TODO: invalid char!!!!!
-								System.err.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@ invalid char: " + decipheredChar + "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+								charCheck = false;
+								//System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@ invalid char: " + decipheredChar + "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
 							}
 						}
 						
 						String[] decipheredWords = decipheredStr.split(" ");
 						int decipheredWordsSize = decipheredWords.length;
-						for (int i  = 0; i < NUM_WORD_CHECK && i < decipheredWordsSize; ++i) {
-							String decipheredWord = decipheredWords[i];
-							if (wordMap.get(decipheredWord.trim()) == null) {
+						for (int i  = 1; i < NUM_WORD_CHECK + 1 && i < decipheredWordsSize; ++i) {
+							String decipheredWord = decipheredWords[i].replace('\r',' ').trim().toLowerCase();
+							if (!wordMap.containsKey(decipheredWord)) {
 								//TODO: invalid word!!!!!
-								System.err.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@ invalid word: " + decipheredWord + "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+								wordCheck = false;
+								//System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@ invalid word: " + decipheredWord + "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
 							}
 						}
 						
-						System.out.println(decipheredStr);
-						System.out.println();
+						if (wordCheck)
+						{
+							// We have deciphered the text! Now to output the text.
+							//toOutputFile.write(decipheredStr);
+							//toOutputFile.write("I HAVE DECIPHERED THE TEXT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+							//toOutputFile.newLine();
+							System.out.println(decipheredStr);
+							System.out.println("I HAVE DECIPHERED THE TEXT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+							//toOutputFile.flush();
+							break;
+						}
 					}
 				} while ( cg.hasMore() );
-				
+				// At this point, we failed to find the key
+				//toOutputFile.write("I'M A NOOB!");
 			}
-		} catch (Exception e) {
+		} 
+		catch (Exception e) 
+		{
 			e.printStackTrace();
+		} finally 
+		{
+			toOutputFile.close();
 		}
 	}
 	
