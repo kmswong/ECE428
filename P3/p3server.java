69737731cf73b00ff07eab7247d0c5fd70ae3658
Xiao Long Lu <xllu@eceLinux1.uwaterloo.ca>
2010-07-18 17:28:14 -0400

    the brute force works upto n=4 within 5 minutes...

--- P3/p3server.java
+++ P3/p3server.java
@@ -11,16 +11,21 @@ import java.net.*;
 import javax.crypto.*;
 import javax.crypto.spec.*;
 import java.util.*;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharacterCodingException;
+
 
 public class p3server 
 {
 	private static final int ERROR_CODE = -1;
 	private static final String TXT_KEY_CHANGE = "KEY CHANGE";
 	private static final String FILENAME_WORDS = "hamlet_word_out.txt";
-	private static final String FILENAME_CHARS = "hamlet_char_out.txt";
 	private static final String FILENAME_OUTDAT = "out.dat";
 	private static final int NUM_WORD_CHECK = 5;
-	private static final int NUM_CHAR_CHECK = 10;
+	private static final boolean DEBUG = false;
 	
 	public static void main(String[] args) throws IOException {
 		int f = Integer.parseInt(args[0]);
@@ -28,12 +33,11 @@ public class p3server
 		DatagramSocket serverSocket = null;
 		
 		HashMap<String, Integer> wordMap = getWordMap(FILENAME_WORDS);
-		HashMap<String, Integer> charMap = getWordMap(FILENAME_CHARS);
-		// HACK: add empty space.
-		charMap.put(" ", 0);
+		
 		
 		try 
 		{
+			// Set up the server socket and port
 		    serverSocket = new DatagramSocket();
 		    serverSocket.setReuseAddress(true);
 		}
@@ -43,10 +47,8 @@ public class p3server
 		    System.exit(ERROR_CODE);
 		}
 		
-		// write the port number to the environemnt variable P
-		System.out.println("Server's port: " + serverSocket.getLocalPort());
-
 		try {
+			// Fork a process that runs the client program with the server's port as an input parameter
 			Integer port = new Integer(serverSocket.getLocalPort());
 			ProcessBuilder pb = new ProcessBuilder( "/home/tripunit/p3client", "-s", port.toString(), "-f", args[0], "-n", args[1]);
 			pb.start();
@@ -54,12 +56,11 @@ public class p3server
 			System.err.println( e.getMessage() );
 		}
 		
-		// Open File for out.dat
-		File file = new File(FILENAME_OUTDAT);
+		// Open File for out.da
 		FileWriter fstream = new FileWriter(FILENAME_OUTDAT);
 		BufferedWriter toOutputFile = new BufferedWriter(fstream);
-
-		System.out.println("Receiving~~");
+		
+		byte[] correctKey = null;
 
 		try {
 			DatagramPacket packet = null;
@@ -89,71 +90,83 @@ public class p3server
 				System.arraycopy(receivedByte , 0, byteToDecrypt , 0, packet.getLength());
 				String receivedData = new String(byteToDecrypt);
 				
-				System.out.println( "+++++++++++++++++++++++++++++++++++++++NEW PACKET+++++++++++++++++++++++++");
-				System.out.println( receivedData );
-				System.out.println( packet.getLength() + " " + receivedByte.length );
+				if (DEBUG) {
+					System.out.println( "+++++++++++++++++++++++++++++++++++++++NEW PACKET+++++++++++++++++++++++++");
+					//System.out.println( receivedData );
+					System.out.println( packet.getLength() + " " + receivedByte.length );
+				}
 				
 				
 				// TODO: Change from continue to SOMETHING ELSE.
 				if ((receivedData.trim()).equals(TXT_KEY_CHANGE)) {
+					correctKey = null;
 					continue;
 				}
 				
-				System.out.println();	
-				cg.reset();
-				do {
-					byte[] key = nextKey(cg);
-
-					c.init( Cipher.DECRYPT_MODE, new SecretKeySpec( key, "AES" ), ivs );
+				if (correctKey == null) {
+					cg.reset();
+					do {
+						// Decrypt the receive packet with the next generated key
+						byte[] key = nextKey(cg);
+						c.init( Cipher.DECRYPT_MODE, new SecretKeySpec( key, "AES" ), ivs );
+						byte[] deciphered = c.doFinal( byteToDecrypt  );
+						String decipheredStr = new String( deciphered );
+						
+						if (DEBUG) {
+							/*
+							System.out.println();
+							System.out.println("Deciphered Text *************************************");					
+							System.out.println(decipheredStr);
+							*/
+						}
+						
+						boolean check = pCheck( deciphered );
+						
+						if (DEBUG) {
+							System.out.println("pCheck: " + check);
+						}
+						
+						// Do char and word checking.
+						if( check && isPureAscii(decipheredStr)) {							
+							boolean wordCheck = true;
+							
+							String[] decipheredWords = decipheredStr.split(" ");
+							int decipheredWordsSize = decipheredWords.length;
+							for (int i  = 1; i < NUM_WORD_CHECK + 1 && i < decipheredWordsSize; ++i) {
+								String decipheredWord = decipheredWords[i].replace('\r',' ').trim().toLowerCase();
+								
+								if (!wordMap.containsKey(decipheredWord)) {
+									//TODO: invalid word!!!!!
+									wordCheck = false;
+									//System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@ invalid word: " + decipheredWord + "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+								}
+							}
+							
+							if (wordCheck)
+							{
+								correctKey = key;
+								break;
+							}
+						}
+					} while ( cg.hasMore() );
+				}
+				
+				if (correctKey != null) {
+					c.init( Cipher.DECRYPT_MODE, new SecretKeySpec( correctKey, "AES" ), ivs );
 					byte[] deciphered = c.doFinal( byteToDecrypt  );
 					String decipheredStr = new String( deciphered );
+											
+					// We have deciphered the text! Now to output the text.
+					toOutputFile.write(decipheredStr);
+					//toOutputFile.write("I HAVE DECIPHERED THE TEXT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+					toOutputFile.newLine();
+					toOutputFile.flush();
 					
-					//System.out.println();
-					//System.out.println("Deciphered Text *************************************");					
-					//System.out.println(decipheredStr);
-					
-					boolean check = pCheck( deciphered );
-					System.out.println("pCheck: " + check);
-					
-					// Do char and word checking.
-
-					if( check ) {
-						boolean wordCheck = true;
-						boolean charCheck = true;
-						int decipheredStrSize = decipheredStr.length();
-						for (int i  = 0; i < NUM_CHAR_CHECK && i < decipheredStrSize; ++i) {
-							String decipheredChar = String.valueOf(decipheredStr.charAt(i));
-							if (!charMap.containsKey(decipheredChar)) {
-								//TODO: invalid char!!!!!
-								charCheck = false;
-								//System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@ invalid char: " + decipheredChar + "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-							}
-						}
-						
-						String[] decipheredWords = decipheredStr.split(" ");
-						int decipheredWordsSize = decipheredWords.length;
-						for (int i  = 1; i < NUM_WORD_CHECK + 1 && i < decipheredWordsSize; ++i) {
-							String decipheredWord = decipheredWords[i].replace('\r',' ').trim().toLowerCase();
-							if (!wordMap.containsKey(decipheredWord)) {
-								//TODO: invalid word!!!!!
-								wordCheck = false;
-								//System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@ invalid word: " + decipheredWord + "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-							}
-						}
-						
-						if (wordCheck)
-						{
-							// We have deciphered the text! Now to output the text.
-							//toOutputFile.write(decipheredStr);
-							//toOutputFile.write("I HAVE DECIPHERED THE TEXT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-							//toOutputFile.newLine();
-							System.out.println(decipheredStr);
-							System.out.println("I HAVE DECIPHERED THE TEXT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-							//toOutputFile.flush();
-							break;
-						}
+					if (DEBUG) {
+						System.out.println(decipheredStr);
+						System.out.println("I HAVE DECIPHERED THE TEXT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 					}
-				} while ( cg.hasMore() );
+				}
 				// At this point, we failed to find the key
 				//toOutputFile.write("I'M A NOOB!");
 			}
@@ -186,6 +199,20 @@ public class p3server
 		return retKey;
 	}
 
+	public static boolean isPureAscii(String v) {
+		byte bytearray []  = v.getBytes();
+		CharsetDecoder d = Charset.forName("US-ASCII").newDecoder();
+		try {
+			CharBuffer r = d.decode(ByteBuffer.wrap(bytearray));
+			r.toString();
+		}
+		catch(CharacterCodingException e) {
+			return false;
+		}
+		return true;
+	}
+
+  
 	private static boolean pCheck(byte[] data)
 	{
 		int dataLength = data.length;
